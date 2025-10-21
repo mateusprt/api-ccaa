abc852196894dd677bab0e168f5ffee500272fd2
mateusprt <matnun95@gmail.com>
2025-10-21 11:56:54 -0300

    Add routes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
-const sum = (a: number, b: number): number => a+b
+import express, { Request, Response } from "express";
+import dotenv from 'dotenv'
+import pgp from "pg-promise";
 
-const result: number = sum(2, 2)
-console.log(result)
+async function main () {
 
+    dotenv.config();
 
-export {
-  sum
+    const {
+      DB_TYPE,
+      DB_USER,
+      DB_PASSWORD,
+      DB_HOST,
+      DB_NAME
+    } = process.env;
+
+    if (!DB_TYPE || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_NAME) {
+      throw new Error('Missing required environment variables for database connection.')
+    }
+
+    const connection = pgp()(`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
+    const app = express();
+    app.use(express.json());
+    
+    app.post("/signup", async (req: Request, res: Response) => {
+        const accountId = crypto.randomUUID();
+        await connection.query("insert into ccca.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)", [accountId, req.body.name, req.body.email, req.body.document, req.body.password]);
+        res.json({
+            accountId
+        });
+    });
+
+    app.get("/accounts/:accountId", async (req: Request, res: Response) => {
+        const [accountData] = await connection.query("select * from ccca.account where account_id = $1", [req.params.accountId]);
+        res.json(accountData);
+    })
+
+    app.listen(3000);
 }
+main();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,6 +1,22 @@
-import { sum } from "../src/main";
+import axios from "axios";
 
-test('Deve somar dois números', () => {
-  const result = sum(2, 2)
-  expect(result).toBe(4)
-})
+test("Deve criar uma conta", async () => {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "97456321558",
+        password: "asdQWE123"
+    }
+    const responseSignup = await axios.post("http://localhost:3000/signup", input);
+    expect(responseSignup.status).toBe(200);
+
+    const outputSignup = responseSignup.data;
+    expect(outputSignup.accountId).toBeDefined();
+    
+    const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`);
+    const outputGetAccount = responseGetAccount.data;
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
+});
