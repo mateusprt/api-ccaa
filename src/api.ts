18aa6b5d4284669d57c581d4877fc595ec21e8b7
mateusprt <matnun95@gmail.com>
2025-12-31 16:15:57 -0300

    Decouple business logic from infrastructure concerns

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import cors from 'cors'
+import { getAccount, signUp } from "./main";
+
+async function main () {
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+    
+    app.post("/signup", async (req: Request, res: Response) => {
+      try {
+        const input = req.body;
+        const output = await signUp(input)
+        res.json(output);
+      } catch (err: any) {
+        res.status(422).json({ message: err.message });
+      }
+    });
+
+    app.get("/accounts/:accountId", async (req: Request, res: Response) => {
+      const accountId = req.params.accountId || '';
+      const output = await getAccount(accountId);
+      res.json(output);
+    })
+
+    app.listen(3000);
+}
+main();
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,48 @@
-import express, { Request, Response } from "express";
 import dotenv from 'dotenv'
 import pgp from "pg-promise";
 import { validateCpf } from "./validateCpf";
 
-async function main () {
+dotenv.config();
 
-    dotenv.config();
+const {
+  DB_TYPE,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME
+} = process.env;
 
-    const {
-      DB_TYPE,
-      DB_USER,
-      DB_PASSWORD,
-      DB_HOST,
-      DB_NAME
-    } = process.env;
-
-    if (!DB_TYPE || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_NAME) {
-      throw new Error('Missing required environment variables for database connection.')
-    }
-
-    const connection = pgp()(`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
-    const app = express();
-    app.use(express.json());
-    
-    app.post("/signup", async (req: Request, res: Response) => {
-        const accountId = crypto.randomUUID();
-
-        if(!req.body.name || !req.body.name.match(/[a-zA-Z ]+ [a-zA-Z ]+/)) {
-          return res.status(422).json({ message: "Invalid name" });
-        }
-
-        if(!req.body.email || !req.body.email.match(/.+@.+\..+/)) {
-          return res.status(422).json({ message: "Invalid email" });
-        }
-
-        if(!req.body.document || !validateCpf(req.body.document)) {
-          return res.status(422).json({ message: "Invalid document" });
-        }
-
-        if(!req.body.password || req.body.password.length < 8 || !req.body.password.match(/[a-z]/) || !req.body.password.match(/[A-Z]/) || !req.body.password.match(/[0-9]/)) {
-          return res.status(422).json({ message: "Invalid password" });
-        }
-
-        await connection.query("insert into ccca.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)", [accountId, req.body.name, req.body.email, req.body.document, req.body.password]);
-        res.json({
-            accountId
-        });
-    });
-
-    app.get("/accounts/:accountId", async (req: Request, res: Response) => {
-        const [accountData] = await connection.query("select * from ccca.account where account_id = $1", [req.params.accountId]);
-        res.json(accountData);
-    })
-
-    app.listen(3000);
+if (!DB_TYPE || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_NAME) {
+  throw new Error('Missing required environment variables for database connection.')
 }
-main();
+
+const connection = pgp()(`${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
+
+export async function signUp(input: any) {
+  const accountId = crypto.randomUUID();
+  if(!input.name || !input.name.match(/[a-zA-Z ]+ [a-zA-Z ]+/)) {
+    throw new Error( "Invalid name" );
+  }
+
+  if(!input.email || !input.email.match(/.+@.+\..+/)) {
+    throw new Error( "Invalid email" );
+  }
+
+  if(!input.document || !validateCpf(input.document)) {
+    throw new Error( "Invalid document" );
+  }
+
+  if(!input.password || input.password.length < 8 || !input.password.match(/[a-z]/) || !input.password.match(/[A-Z]/) || !input.password.match(/[0-9]/)) {
+    throw new Error( "Invalid password" );
+  }
+
+  await connection.query("insert into ccca.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)", [accountId, input.name, input.email, input.document, input.password]);
+  return {
+      accountId
+  };
+}
+
+export async function getAccount(accountId: string) {
+  const [accountData] = await connection.query("select * from ccca.account where account_id = $1", [accountId]);
+  return accountData;
+}
